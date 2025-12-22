0bead9288f9eecd52b59c5a5d0479263b7d51175
humanoid-human <trevorxu5@gmail.com>
2025-12-22 14:21:16 -0700

    minor optimization, timing

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,27 @@
-use std::io;
+use std::{io::stdin, time::Instant};
 use rayon::prelude::*;
 
 const TN_SEED: i64 = -8080661144804004702;
 
 fn main() {
-    let seed: i64;
-    println!("Input seed (leave blank for TechNut seed): ");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("failed to read line");
-    if input.len() == 1 {
-        seed = TN_SEED;
-    } else {
-        seed = input.strip_suffix("\n").unwrap().parse().expect("failed to read seed");
-    }
-
-    let step = get_i32("Step size", 10);
-    let range = get_i32("Range", 30000);
-    let target = get_i32("Maximum slime-spawnable blocks", 700);
+    let seed = get_i64("Seed", TN_SEED);
+    let step = get_i64("Step size", 10) as i32;
+    let range = get_i64("Range", 30000) as i32;
+    let target = get_i64("Maximum slime-spawnable blocks", 700) as i32;
 
     println!("Calculating");
 
+    let tic = Instant::now();
     print_mins(seed, step, range, target);
+    let toc = tic.elapsed();
+
+    println!("Finished in {}ms", toc.as_millis());
 }
 
-fn get_i32(print: &str, default: i32) -> i32 {
+fn get_i64(print: &str, default: i64) -> i64 {
     println!("{} (leave blank for {}): ", print, default);
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("failed to read from stdin");
+    stdin().read_line(&mut input).expect("failed to read from stdin");
     if input.len() == 1 {
         default
     } else {
@@ -57,31 +52,29 @@ fn slime_spawnable_nearby(seed: i64, x: i32, z: i32, max: i32) -> i32 {
     let cx = x >> 4;
     let cz = z >> 4;
 
-    for dcx in -9..10 {
-        for dcz in -9..10 {
-            if !is_slime_chunk(seed, cx + dcx, cz + dcz) {
+    for dcx in -9..=9 {
+        for dcz in -9..=9 {
+            let cd = dcx * dcx + dcz * dcz;
+            if cd > 85 || !is_slime_chunk(seed, cx + dcx, cz + dcz) {
                 continue;
             }
-            let cd = dcx * dcx + dcz * dcz;
+
             if cd <= 45 && cd > 5 {
                 out += 256;
-                if out > max {
-                    return out;
-                }
-            } else {
-                for dx in 0..16 {
-                    for dz in 0..16 {
-                        let dist_x = (dcx << 4) + dx;
-                        let dist_z = (dcz << 4) + dz;
-                        let d2 = (dist_x * dist_x) + (dist_z * dist_z);
-                        if d2 <= 128 * 128 && d2 >= 24 * 24 {
-                            out += 1;
-                            if out > max {
-                                return out;
-                            }
-                        }
+                if out > max { return out; }
+                continue;
+            }
+
+            for dx in 0..16 {
+                for dz in 0..16 {
+                    let dist_x = (dcx << 4) + dx;
+                    let dist_z = (dcz << 4) + dz;
+                    let d2 = (dist_x * dist_x) + (dist_z * dist_z);
+                    if d2 <= 128 * 128 && d2 >= 24 * 24 {
+                        out += 1;
                     }
                 }
+                if out > max { return out; }
             }
         }
     }
